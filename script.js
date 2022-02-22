0bdccba4b99fb26be7bb9c89719d1880d4f55b10
Julien Az <julien.azbrg@gmail.com>
2022-02-22 11:13:09 +0100

    tp3 js presentation

---
+++ script.js
@@ -0,0 +1,9 @@
+// Créer un événement au scroll
+
+// Réduire la navbar quand on descend vers le vite, la remettre à sa taille initiale si on remonte tout en haut
+
+// Faire apparaitre l'image de la partie improvise
+
+// Faire apparaitre la popup quand on est en bas du site
+
+// Bonus : quand on clicke sur la popup elle disparait pour toujours
