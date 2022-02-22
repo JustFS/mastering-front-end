8275e6df29539cf1c848be825252ba239716080a
Julien Az <julien.azbrg@gmail.com>
2022-02-22 11:29:24 +0100

    tp3 js correction

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-// Créer un événement au scroll
+let playOnce = true;
 
-// Réduire la navbar quand on descend vers le vite, la remettre à sa taille initiale si on remonte tout en haut
+window.addEventListener("scroll", () => {
+  // Navbar effect
+  if (window.scrollY > 50) {
+    navbar.style.height = "45px";
+  } else {
+    navbar.style.height = "90px";
+  }
 
-// Faire apparaitre l'image de la partie improvise
+  let scrollValue =
+    (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+  console.log(scrollValue);
+  // Image
+  if (scrollValue > 0.45) {
+    imgImprovise.style.opacity = 1;
+    imgImprovise.style.transform = "none";
+  }
 
-// Faire apparaitre la popup quand on est en bas du site
+  // Popup
+  if (scrollValue > 0.85 && playOnce) {
+    popup.style.opacity = 1;
+    popup.style.transform = "none";
+    playOnce = false;
+  }
+});
 
-// Bonus : quand on clicke sur la popup elle disparait pour toujours
+closeBtn.addEventListener("click", () => {
+  popup.style.opacity = 0;
+  popup.style.transform = "translateX(500px)";
+});
