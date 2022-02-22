a0744e67bf81ebb4b907820f6de8a0e911f12124
Julien Az <julien.azbrg@gmail.com>
2022-02-22 14:12:44 +0100

    tp7 js presentation

---
+++ index.js
@@ -0,0 +1,10 @@
+// Créer la logique de compte à rebours
+
+choice.value = 5 minutes
+
+choice.value * 60 = totalSeconds
+
+let minutes = Math.floor(totalSeconds / 60);
+let seconds = totalSeconds % 60
+
+// Créer un événement à la validation du form pour lancer le compte à rebours
