e3f2bcfbf120b9c2fd8a1027c9ceafd040ed8d85
Julien Az <julien.azbrg@gmail.com>
2022-02-22 11:41:00 +0100

    tp4 js presentation

---
+++ index.js
@@ -0,0 +1,9 @@
+// Créer un événement au click sur le hamburger menu pour sortir la sidebar, ranger la sidebar si on reclick dessus
+
+// Ranger la sidebar si on click sur le contenu principal
+
+// BONUS
+// Créer en css l'hamburger menu qui se transforme en croix
+
+// Aide
+// Priorités en CSS : id > classe > baliseHtml
