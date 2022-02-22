d27751ac4f533e033f58e757c4faf9eec680206a
Julien Az <julien.azbrg@gmail.com>
2022-02-22 11:01:35 +0100

    tp2 js correction

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-// Créer un événément au scroll
+let lastScroll = 0;
 
-// Cacher la navbar si l'utilisateur comment à descendre et la sortir quand il remonte
+window.addEventListener("scroll", () => {
+  if (window.scrollY < lastScroll) {
+    navbar.style.top = 0;
+  } else {
+    navbar.style.top = "-60px";
+  }
 
-// Stocker la valeur du précédent niveau de scroll pour savoir si l'on est monté ou descendu
-
-// Connaitre niveau de scroll (window.scrollY)
+  lastScroll = window.scrollY;
+});
