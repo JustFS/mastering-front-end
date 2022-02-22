7ae9b043e3645a14c63048c3ab24234b0bbfb994
Julien Az <julien.azbrg@gmail.com>
2022-02-22 14:29:13 +0100

    tp6 js correction

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-// Créer la logique de compte à rebours
+let totalSeconds;
+let interval;
 
-choice.value = 5 minutes
+function countDown() {
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
 
-choice.value * 60 = totalSeconds
+  const sec = seconds < 10 ? "0" + seconds : seconds;
 
-let minutes = Math.floor(totalSeconds / 60);
-let seconds = totalSeconds % 60
+  countdownDisplay.textContent = `${minutes} : ${sec}`;
 
-// Créer un événement à la validation du form pour lancer le compte à rebours
+  if (totalSeconds > 0) {
+    totalSeconds--;
+  } else {
+    countdownDisplay.textContent = "C'est terminé !";
+    clearInterval(interval);
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (isNaN(choice.value)) {
+    alert("Entrez un chiffre avant que je me fâche");
+  } else {
+    totalSeconds = choice.value * 60;
+    choice.value = "";
+    clearInterval(interval);
+    interval = setInterval(countDown, 1000);
+  }
+});
