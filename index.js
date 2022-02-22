f8dac37d23cbc8c37835a058ca86b49541cc5f89
Julien Az <julien.azbrg@gmail.com>
2022-02-22 10:41:38 +0100

    tp1 js correction

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-// Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
+const mouses = document.querySelectorAll(".mouse");
 
-// Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
+window.addEventListener("mousemove", (e) => {
+  mouses.forEach((mouse) => {
+    mouse.style.top = e.y + "px";
+    mouse.style.left = e.x + "px";
+  });
+});
 
-// S'assurer que les liens sont clickables
+// window.addEventListener("mousemove", (e) => {
+//   cursor.style.top = e.y + "px";
+//   cursor.style.left = e.x + "px";
 
-// Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
+//   mouse1.style.top = e.y + "px";
+//   mouse1.style.left = e.x + "px";
+
+//   mouse2.style.top = e.y + "px";
+//   mouse2.style.left = e.x + "px";
+// })
