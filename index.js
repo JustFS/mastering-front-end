059495be003aee193bd73335009551d6fc72d882
Julien Az <julien.azbrg@gmail.com>
2022-02-22 10:08:42 +0100

    td1 js presentation

---
+++ index.js
@@ -0,0 +1,5 @@
+// Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
+
+// Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
+
+// Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
