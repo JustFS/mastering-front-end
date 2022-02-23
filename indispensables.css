9fed7b488c228e67fb01dbf5ac1930b80aa48900
Julien Az <julien.azbrg@gmail.com>
2022-02-23 15:28:35 +0100

    indispensables css

---
+++ indispensables.css
@@ -0,0 +1,60 @@
+/* Centrer un élément horizontalement parfaitement  */
+/* Si l'élément est en block */
+.titre {
+  text-align: center;
+}
+.boite {
+  margin: 0 auto;
+  display: block; /* Si jamais il n'est pas en block de base (img, a, span, button) */
+}
+
+/* **************** FLEX ******************* */
+
+/* Centrer un élément dans sa boite (enfant unique) */
+.parent-d-1-element {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+/* Répartir équitablement sur la même ligne des éléments dans la page */
+.parent-de-plusieurs-enfants {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+/* ***************** GRID ******************** */
+
+/* Répartir précisemment des éléments dans la largeur avec des tailles différentes */
+.parent-de-3-elements {
+  display: grid;
+  grid-template-columns: 25% 55% 20%;
+}
+
+/* Créer des grilles complexes */
+.parent-des-elements {
+  display: grid;
+  grid-template-columns: 100px 1fr 1fr;
+  grid-template-rows: 1fr 1fr 300px;
+  grid-template-areas:
+    "header header header"
+    "side main main"
+    "side footer footer";
+}
+.enfant1 {
+  grid-area: header;
+}
+
+/* ***************** ABSOLUTE ***************** */
+
+/* Centrer parfaitement un élément */
+.parent {
+  position: relative;
+}
+.enfant {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
