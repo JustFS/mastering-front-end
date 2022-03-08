74d3c4020c328c195e10353ae8479f7b600e49ea
Julien Az <julien.azbrg@gmail.com>
2022-03-08 21:09:40 +0100

    app1 react support

--- README.md
+++
@@ -1,5 +0,0 @@
-service mail : service_hgyryh5
-template mail : template_5ni7fmu
-
-User ID : bsgpx6f3M2Y83DixN
-Access Token : hyCJPYM0OZ3_gK0aUJecX
---
+++ src/data/projectsData.js
@@ -0,0 +1,42 @@
+export const projectsData = [
+  {
+    id: 1,
+    title: "Casta",
+    date: "Janvier 2020",
+    languages: ["React", "Php", "Sass"],
+    infos:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
+    img: "./assets/img/projet-2.jpg",
+    link: "http://www.google.com",
+  },
+  {
+    id: 2,
+    title: "Lyon Béton",
+    date: "Mars 2020",
+    languages: ["Symfony", "Vue"],
+    infos:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
+    img: "./assets/img/projet-1.jpg",
+    link: "http://www.google.com",
+  },
+  {
+    id: 3,
+    title: "Everpost",
+    date: "Avril 2020",
+    languages: ["Wordpress", "Php", "React"],
+    infos:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
+    img: "./assets/img/projet-3.jpg",
+    link: "http://www.google.com",
+  },
+  {
+    id: 4,
+    title: "Creative Dev",
+    date: "Juillet 2020",
+    languages: ["Vue", "Php"],
+    infos:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas cumque labore suscipit, pariatur laboriosam autem omnis saepe quisquam enim iste.",
+    img: "./assets/img/projet-4.jpg",
+    link: "http://www.google.com",
+  },
+];
