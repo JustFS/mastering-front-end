19459e155b2af9fa9ee3a6576c4a34ccfdfee81e
Julien Az <julien.azbrg@gmail.com>
2022-03-09 19:19:26 +0100

    app2 react demarrage

--- src/components/Logo.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Logo = () => {
-  return <span className="logo">FS</span>;
-};
-
-export default Logo;
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./style/index.scss";
-
-ReactDOM.render(<App />, document.getElementById("root"));
